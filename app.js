ef1a886d179f9aec50e3bb9ddb5f8623e1d42b99
Emir Tüfekci <e.tufekci@simplydeliver.com>
2024-08-12 22:05:20 +0300

    Basic Login System

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import morgan from 'morgan'
 dotenv.config({ path: './config/.env'})
+
+//DB Connection
+connectDB()
 
 const app = express()
 
 //Middlewares
 app.use(cookieParser())
 app.use(bodyParser.json())
+app.use(morgan('dev')) //HTTP Request Logger
+
+//Routes
+import authRoute from './routes/auth.js'
+
+app.use('/auth',authRoute)
 
 app.listen(process.env.PORT || 5000,() => {
     console.log(`Server is running on Port ${process.env.PORT}.`)
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,43 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+
 const router = express.Router()
 
-router.post('/login', async (req,res) =>{
-    res.json('hi')
+router.post('/register', async (req,res) =>{
+    try {
+        const {username,emailaddress,password} = req.body
+        const userCheck = await User.findOne({username,emailaddress})
+
+        if(!userCheck){
+            const newUser = await User.create({username,emailaddress,password})
+            res.json(newUser)
+        }
+        else{            
+            res.status(400).json({error:'userexists'})
+        }
+    } catch (error) {
+        res.status(500).json({error})
+    }
 })
+
+router.post('/login', async (req,res) => {
+    try {
+        const {username,password} = req.body
+
+        const user = await User.findOne({username,password})
+        if(user){
+            var token = jwt.sign({ userName:user.userName,userId:user._id }, process.env.JWTSECRET, { algorithm: 'RS256' },(err,token) => {
+                if(err) throw new Error
+                res.cookie('token',token).json({id:user._id,userName:user.username})
+            });
+        }
+        else{
+            res.status(400).json({error:'usernotfound'})
+        }
+    } catch (error) {
+        res.status(500).json({error})
+    }
+})
+
+export default router
