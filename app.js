232f0c849556a0d67495061f52b480af6c85a512
Emir Tüfekci <e.tufekci@simplydeliver.com>
2024-08-12 20:51:38 +0300

    Server created

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello')
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv/config'
+
+const app = express()
+
+//Middlewares
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+app.listen(process.env.PORT || 5000,() => {
+    console.log(`Server is running on Port ${process.env.PORT}.`)
+})
